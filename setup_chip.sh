a5550d5b48b926b785baef262d60edcb39ba0ba3
Ryno <rynojvr@gmail.com>
2016-12-29 17:19:28 -0800

    Add some pauses to setup script for debugging

--- setup_chip.sh
+++ setup_chip.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+function pause_prompt {
+	read -p $1;
+}
+
 sudo apt-get update;
 #sudo apt-get install -y \
+
+pause_prompt "Just ran Apt-get Update. Next is SSHKEYGEN"
 
 # ----- Create and Add SSH Keys -----
 ssh-keygen -t rsa -b 2048 -N "" -f .ssh/id_rsa
 ssh-copy-id rynojvr@rynojvr.com	
+
+pause_prompt "Finished SSHKEYGEN stuff. AutoSSH config next"
 
 # ----- Setup AutoSSH -----
 sudo cp autossh.service /lib/systemd/system/
@@ -14,4 +22,4 @@ sudo cp root/bin/start_autossh.sh /root/bin/
 sudo chmod +x /root/bin/start_autossh.sh
 sudo systemctl enable autossh
 
-
+pause_prompt "Finished setting up AutoSSH"
