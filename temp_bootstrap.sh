cb28fbc9e6305e4d73ddacc1bbeedefc0783740a
Ryno Janse van Rensburg <rynojvr@gmail.com>
2017-05-14 17:08:30 -0700

    Create and chown home directory

--- temp_bootstrap.sh
+++ temp_bootstrap.sh
@@ -3,5 +3,7 @@
 useradd -d /home/rynojvr rynojvr
 passwd
 visudo
+mkdir /home/rynojvr
+chown rynojvr /home/rynojvr
 
 su - rynojvr -c "mkdir -p /home/rynojvr/dev/github/rynojvr; git clone https://github.com/rynojvr/scripts; cd scripts/; bash setup.sh"
