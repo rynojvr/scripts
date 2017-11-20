331b81745311b09f22841e805090d3df47b32d20
Ryno <rynojvr@gmail.com>
2017-11-20 18:38:59 +0100

    Add setup script for mana chip

---
+++ chip/setup_chip_mana_toolkit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash	
+
+INSTALL_DIR=$HOME/dev/github/rynojvr
+
+mkdir -p $INSTALL_DIR
+cd $INSTALL_DIR
+git clone https://github.com/rynojvr/scripts
+cd scripts/
+
+./link-files.sh -v -f
+
+# --------- NodeJS --------- 
+# https://nodejs.org/en/download/package-manager/
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo apt-get install build-essentials
+
+# --------- Mana ---------
+
+cd $INSTALL_DIR
+git clone https://github.com/rynojvr/mana
+cd mana
+./chip-install.sh
+
+sudo apt-get install vim
