86002045f3d35689b735c147864e746b263bd648
Ryno <rynojvr@gmail.com>
2017-11-20 21:06:08 +0100

    Add gcc dep

--- chip/setup_chip_mana_toolkit.sh
+++ chip/setup_chip_mana_toolkit.sh
@@ -15,7 +15,7 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo apt-get install -y build-essentials
-sudo apt-get install -y make
+sudo apt-get install -y make gcc
 
 # --------- Mana ---------
 
