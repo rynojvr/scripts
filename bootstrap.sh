3a140b9f6da663ae86c8fef94cc32fb7f233ff4c
Ryno <rynojvr@gmail.com>
2017-12-08 22:54:04 +0100

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Hello, World"
+# Get the SaltStack bootstrap
+if command -v curl 2>/dev/null; then
+	curl -L https://bootstrap.saltstack.com -o install_salt.sh
+else
+	wget https://bootstrap.saltstack.com -O install_salt.sh
+fi
+
+sudo sh install_salt.sh -A 159.203.180.72
