d46efef763ce932bc86559416b8c773b534caacb
Ryno <rynojvr@gmail.com>
2017-12-08 23:32:38 +0100

    Specify hostname during setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo -n "Enter new hostname: "
+read new_hostname
+echo "Setting hostname to [$new_hostname]"
+hostnamectl set-hostname $new_hostname
+
 # Get the SaltStack bootstrap
 if command -v curl 2>/dev/null; then
 	curl -L https://bootstrap.saltstack.com -o install_salt.sh
